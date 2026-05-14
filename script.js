8c6c2dc7e68ea929db8012c74e5dea8c407cb547
Cipher95 <h.alobaidan95@gmail.com>
2026-05-14 20:50:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -139,9 +139,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const html = `
             <div class="content-wrapper">
-                <div class="content-image">
-                    <img src="${data.image}" alt="${data.title}">
-                </div>
 
                 <div class="content-text">
                     <h2>${data.title}</h2>
